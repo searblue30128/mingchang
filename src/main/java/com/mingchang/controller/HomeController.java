a56c3066e2872f0e58fbb17b44de2734cb711c00
Van Yang <vanyang@iisigroup.com>
2019-11-22 14:43:39 +0800

    fix redirect addImg

--- src/main/java/com/mingchang/controller/HomeController.java
+++ src/main/java/com/mingchang/controller/HomeController.java
@@ -20,69 +20,69 @@ import com.mingchang.service.ImageCardService;
 @RequestMapping("/home")
 public class HomeController {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    private ImageCardService imageCardService;
+	@Autowired
+	private ImageCardService imageCardService;
 
-    @RequestMapping("/")
-    public String homepage(Map<String, Object> map) {
-        // connect database way
-        // https://www.jetbrains.com/help/datagrip/how-to-connect-to-heroku-postgres.html
-        List<ImageCard> listImageCard = imageCardService.listImageCard();
-        map.put("imageCardList", listImageCard);
-        logger.debug(listImageCard.toString());
-        return "home";
-    }
+	@RequestMapping("/")
+	public String homepage(Map<String, Object> map) {
+		// connect database way
+		// https://www.jetbrains.com/help/datagrip/how-to-connect-to-heroku-postgres.html
+		List<ImageCard> listImageCard = imageCardService.listImageCard();
+		map.put("imageCardList", listImageCard);
+		logger.debug(listImageCard.toString());
+		return "home";
+	}
 
-    @RequestMapping(value = "/addImg", method = RequestMethod.GET)
-    public String addImageCardPage(@ModelAttribute("imageCard") ImageCard imageCard, Map<String, Object> map) {
-        List<ImageCard> listImageCard = imageCardService.listImageCard();
-        map.put("imageCardList", listImageCard);
-        return "addImg";
-    }
+	@RequestMapping(value = "/addImg", method = RequestMethod.GET)
+	public String addImageCardPage(@ModelAttribute("imageCard") ImageCard imageCard, Map<String, Object> map) {
+		List<ImageCard> listImageCard = imageCardService.listImageCard();
+		map.put("imageCardList", listImageCard);
+		return "addImg";
+	}
 
-    @RequestMapping(value = "/addImg", method = RequestMethod.POST)
-    public String addImageCard(@ModelAttribute("imageCard") ImageCard imageCard) {
-        String name = transferUTF8(imageCard.getName());
-        imageCard.setName(name);
-        String description = transferUTF8(imageCard.getDescription());
-        imageCard.setDescription(description);
-        String moreDetail = transferUTF8(imageCard.getMoreDetail());
-        imageCard.setMoreDetail(moreDetail);
-        imageCardService.addImageCard(imageCard);
-        return "redirect:/home/";
-    }
+	@RequestMapping(value = "/addImg", method = RequestMethod.POST)
+	public String addImageCard(@ModelAttribute("imageCard") ImageCard imageCard) {
+		String name = transferUTF8(imageCard.getName());
+		imageCard.setName(name);
+		String description = transferUTF8(imageCard.getDescription());
+		imageCard.setDescription(description);
+		String moreDetail = transferUTF8(imageCard.getMoreDetail());
+		imageCard.setMoreDetail(moreDetail);
+		imageCardService.addImageCard(imageCard);
+		return "addImg";
+	}
 
-    @RequestMapping("/delete/{imgId}")
-    public String deletePerson(@PathVariable("imgId") Integer imgId) {
-        imageCardService.removeImageCard(imgId);
-        return "redirect:/home/";
-    }
+	@RequestMapping("/delete/{imgId}")
+	public String deletePerson(@PathVariable("imgId") Integer imgId) {
+		imageCardService.removeImageCard(imgId);
+		return "redirect:/home/";
+	}
 
-    @RequestMapping("/about")
-    public String about(Map<String, Object> map) {
-        return "about";
-    }
+	@RequestMapping("/about")
+	public String about(Map<String, Object> map) {
+		return "about";
+	}
 
-    @RequestMapping("/service")
-    public String service(Map<String, Object> map) {
-        return "service";
-    }
+	@RequestMapping("/service")
+	public String service(Map<String, Object> map) {
+		return "service";
+	}
 
-    @RequestMapping("/contact")
-    public String contact(Map<String, Object> map) {
-        return "contact";
-    }
+	@RequestMapping("/contact")
+	public String contact(Map<String, Object> map) {
+		return "contact";
+	}
 
-    private String transferUTF8(String str) {
-        String result = "";
-        try {
-            result = new String(str.getBytes("ISO-8859-1"), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            System.out.println(result + " 轉碼失敗 ");
-            e.printStackTrace();
-        }
-        return result;
-    }
+	private String transferUTF8(String str) {
+		String result = "";
+		try {
+			result = new String(str.getBytes("ISO-8859-1"), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			System.out.println(result + " 轉碼失敗 ");
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
