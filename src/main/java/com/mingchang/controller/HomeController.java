aa2a0a276d69ea6a7f97523508aef48f035097d0
りゅうせいき <lk3h5yr@gmail.com>
2019-12-04 00:43:24 +0800

    rollback java code

--- src/main/java/com/mingchang/controller/HomeController.java
+++ src/main/java/com/mingchang/controller/HomeController.java
@@ -60,19 +60,18 @@ public class HomeController {
         return "redirect:/home/addImg";
     }
 
-    @RequestMapping(value = "/showDetail", method = RequestMethod.GET)
-    public String showDetailImageCardPage(@ModelAttribute("imgId") Integer imgId, Map<String, Object> map) {
-        System.out.println("imgId "+imgId);
-        List<ImageCard> listImageCard = imageCardService.listImageCard();
-        ImageCard imgCard = listImageCard
-                                .stream()
-                                .filter(obj -> obj.getId() == imgId)
-                                .findFirst()
-                                .get();
-        map.put("imageCard", imgCard);
-        return "showDetail";
-    }
-
+    //    @RequestMapping(value = "/showDetail", method = RequestMethod.GET)
+    //    public String showDetailImageCardPage(@ModelAttribute("imgId") Integer imgId, Map<String, Object> map) {
+    //        System.out.println("imgId "+imgId);
+    //        List<ImageCard> listImageCard = imageCardService.listImageCard();
+    //        ImageCard imgCard = listImageCard
+    //                                .stream()
+    //                                .filter(obj -> obj.getId() == imgId)
+    //                                .findFirst()
+    //                                .get();
+    //        map.put("imageCard", imgCard);
+    //        return "showDetail";
+    //    }
     @RequestMapping("/about")
     public String about(Map<String, Object> map) {
         return "about";
